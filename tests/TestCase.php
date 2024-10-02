46012e71a341f465ecdef7a2a8da5786b15e9347
Danny Wijffelaars <danny.wijffelaars@syntec-it.nl>
2024-10-02 11:07:17 +0200

    Rename of traits dir

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Evoware\OllamaPHP\Tests;
 
-use Evoware\OllamaPHP\traits_temp\MocksHttpRequests;
+use Evoware\OllamaPHP\Traits\MocksHttpRequests;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
